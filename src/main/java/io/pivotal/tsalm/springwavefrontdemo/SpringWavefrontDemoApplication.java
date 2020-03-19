54a6a5ffe518e817a5bbbba7f655b839af5b7d01
Timo Salm <tsalm@pivotal.io>
2020-03-19 10:30:28 +0100

    Initial project setup

---
+++ src/main/java/io/pivotal/tsalm/springwavefrontdemo/SpringWavefrontDemoApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.tsalm.springwavefrontdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWavefrontDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWavefrontDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pivotal/tsalm/springwavefrontdemo/SpringWavefrontDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pivotal.tsalm.springwavefrontdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWavefrontDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
